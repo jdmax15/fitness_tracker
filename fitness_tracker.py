7d99d966652123f05493c66c2bb57da2d06e3718
jdmax15 <joel.maxwell15@gmail.com>
2024-08-15 06:58:32 +0930

    finished get_weight(), imported datetime

--- fitness_tracker.py
+++ fitness_tracker.py
@@ -1,12 +1,21 @@
+import sys
 
 
 
-
+def get_weight():
+    while True:
+        try:
+            weight = int(input("What is your weight? "))
+            return weight
+        except ValueError:
+            print("Please enter a number only.")
+        
 
 
 
 def main():
-    None
+    weight = get_weight()
+    print(f"Your weight is {weight}kg.")
 
 if __name__ == "__main__":
     main()
