8ef7f37dc34a2a9f33ce0042afe03cf8f5da7cd9
jdmax15 <joel.maxwell15@gmail.com>
2024-08-15 07:24:33 +0930

    added get_weight and get_date functions

--- fitness_tracker.py
+++ fitness_tracker.py
@@ -1,21 +1,33 @@
-import sys
+from datetime import date, datetime
+
+
 
 
 
 def get_weight():
     while True:
         try:
-            weight = int(input("What is your weight? "))
+            weight = float(input("What is your weight? "))
             return weight
         except ValueError:
             print("Please enter a number only.")
-        
 
 
+def get_weight_str(weight, date):
+    return f"Your weight was {weight}kg on {date}."
+
+def get_date():
+    date = datetime.today()
+    date = date.strftime("%A, %d %B %Y at %I:%M%p")
+    return date
 
 def main():
+
+    date = get_date()
     weight = get_weight()
-    print(f"Your weight is {weight}kg.")
+    weight_str = get_weight_str(weight, date)
+    
+    print(weight_str)
 
 if __name__ == "__main__":
     main()
